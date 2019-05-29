a45615a429d1fe5d1cfd0f362aa3c98c78295fb9
Mac24h <mac24h@Yataome.local>
2019-05-29 18:37:41 +0700

    init

--- server.js
+++ server.js
@@ -9,6 +9,36 @@ server.use(express.static('public'))
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({ extended: true }))
 
+server.get("/edit-address", (req, res) => {
+  res.sendFile(__dirname + "/public/edit-address.html")
+})
+
+server.get("/blog", (req, res) => {
+  res.sendFile(__dirname + "/public/blog.html")
+})
+
+server.get("/blogs", (req, res) => {
+  res.sendFile(__dirname + "/public/blogs.html")
+})
+
+server.get("/cart", (req, res) => {
+  res.sendFile(__dirname + "/public/cart.html")
+})
+
+
+server.get("/login", (req, res) => {
+  res.sendFile(__dirname + "/public/login.html")
+})
+
+
+server.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/public/register.html")
+})
+
+server.get("/category", (req, res) => {
+  res.sendFile(__dirname + "/public/category.html")
+})
+
 server.get("/product", (req, res) => {
   res.sendFile(__dirname + "/public/product.html")
 })
